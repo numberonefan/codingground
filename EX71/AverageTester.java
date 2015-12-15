131907e1510c2a95a6545276ab842b2a917d9b6f
numberonefan <likublefugar@ymail.com>
2015-12-15 13:18:04 -0500

    Created by Coding Ground Client

---
+++ EX71/AverageTester.java
@@ -0,0 +1,7 @@
+public class AverageTester{
+
+     public static void main(String []args){
+         System.out.println(AverageFinder.average(2,7,9));
+     // Write code here to test the AverageFinder class
+     }
+}
